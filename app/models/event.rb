eae450f1f22c865d53179a9b754f2b0dd4f3da44
Pradeepan Mohan <pradeep00271@gmail.com>
2021-06-02 13:57:48 +0530

    Edited Search

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
 	def self.search_by(search_term)
-		where("LOWER(event_name) LIKE :search_term OR LOWER(event_description) LIKE :search_term OR LOWER(city) LIKE :search_term OR LOWER(start_date) LIKE :search_term OR LOWER(end_date) LIKE :search_term ",search_term: "%#{search_term.downcase}%")
+		where("LOWER(event_name) LIKE :search_term OR LOWER(event_description) LIKE :search_term OR LOWER(city) LIKE :search_term ",search_term: "%#{search_term.downcase}%")
 	end
 
 	belongs_to :user
